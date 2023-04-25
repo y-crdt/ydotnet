f6ba9131f352fc9bb176eb77111c4ab17ec05751
Lucas Viana <lv201122@gmail.com>
2023-04-24 23:34:27 -0300

    feat(doc): add DocOptions and DocEncoding definitions

---
+++ YDotNet/Document/Options/DocEncoding.cs
@@ -0,0 +1,22 @@
+﻿namespace YDotNet.Document.Options;
+
+/// <summary>
+///     Determines how string length and offsets are calculated for <see cref="Text" /> and <see cref="XmlText" />.
+/// </summary>
+public enum DocEncoding
+{
+    /// <summary>
+    ///     Compute editable strings length and offset using UTF-8 byte count.
+    /// </summary>
+    Utf8 = 1,
+
+    /// <summary>
+    ///     Compute editable strings length and offset using UTF-16 chars count.
+    /// </summary>
+    Uf16 = 2,
+
+    /// <summary>
+    ///     Compute editable strings length and offset using UTF-32 (Unicode) code points number.
+    /// </summary>
+    Utf32 = 3
+}
