2f9df9fa7108c9640933b75d7777a6e3b08be9af
Lucas Viana <lv201122@gmail.com>
2023-04-24 23:38:08 -0300

    feat(doc): add DocChannel.NewWithOptions() and move to new namespace

--- YDotNet/Native/Doc/DocChannel.cs
+++ YDotNet/Native/Doc/DocChannel.cs
@@ -1,11 +1,15 @@
 using System.Runtime.InteropServices;
 
-namespace YDotNet.Native;
+namespace YDotNet.Native.Doc;
 
 internal static class DocChannel
 {
     [DllImport(ChannelSettings.NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ydoc_new")]
     public static extern nint New();
+
+    [DllImport(
+        ChannelSettings.NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ydoc_new_with_options")]
+    public static extern nint NewWithOptions(DocOptionsNative options);
 
     [DllImport(ChannelSettings.NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ydoc_destroy")]
     public static extern void Destroy(nint doc);
