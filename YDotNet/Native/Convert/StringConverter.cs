b0e82adf1612fac9b3fe3fa43384787ade4522a9
Lucas Viana <lv201122@gmail.com>
2023-04-24 23:39:53 -0300

    feat(doc): add StringConverter definition

---
+++ YDotNet/Native/Convert/StringConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace YDotNet.Native.Convert;
+
+public static class StringConverter
+{
+    public static byte[] ToBytes(this string? value)
+    {
+        if (value is null)
+        {
+            return Array.Empty<byte>();
+        }
+
+        return Encoding.UTF8.GetBytes(value);
+    }
+}
