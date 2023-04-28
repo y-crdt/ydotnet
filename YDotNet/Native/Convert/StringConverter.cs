0f4e8da6f75145bf86f7e0e687fdbaebc6a97870
Lucas Viana <lv201122@gmail.com>
2023-04-27 21:10:22 -0300

    feat(converter): add StringConverter.FromUtf8Bytes()

--- YDotNet/Native/Convert/StringConverter.cs
+++ YDotNet/Native/Convert/StringConverter.cs
@@ -1,16 +1,17 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
+using System.Text;
 
 namespace YDotNet.Native.Convert;
 
 public static class StringConverter
 {
-    public static byte[] ToUtf8Bytes(this string? value)
+    public static byte[] ToUtf8Bytes(this string value)
     {
-        if (value is null)
-        {
-            return Array.Empty<byte>();
-        }
-
         return Encoding.UTF8.GetBytes(value);
+    }
+
+    public static string? FromUtf8Bytes(this nint value)
+    {
+        return Marshal.PtrToStringUTF8(value);
     }
 }
