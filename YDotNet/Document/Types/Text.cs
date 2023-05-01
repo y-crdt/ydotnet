6d2e084d7105d7a354355232250b2b76005618e3
Lucas Viana <lv201122@gmail.com>
2023-05-01 19:09:13 -0300

    feat(doc): add Text and TextChannel to support features of Doc class

---
+++ YDotNet/Document/Types/Text.cs
@@ -0,0 +1,35 @@
+using YDotNet.Document.Transactions;
+using YDotNet.Native.Types;
+
+namespace YDotNet.Document.Types;
+
+/// <summary>
+///     A shared data type that represents a text string.
+/// </summary>
+public class Text
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="Text" /> class.
+    /// </summary>
+    /// <param name="handle">The handle to the native resource.</param>
+    internal Text(nint handle)
+    {
+        Handle = handle;
+    }
+
+    /// <summary>
+    ///     Gets the handle to the native resource.
+    /// </summary>
+    internal nint Handle { get; }
+
+    /// <summary>
+    ///     Inserts a string in the given `index`.
+    /// </summary>
+    /// <param name="transaction">The transaction that wraps this write operation.</param>
+    /// <param name="index">The index must be between 0 and <see cref="Length" /> or an exception will be thrown.</param>
+    /// <param name="value">The text to be inserted.</param>
+    public void Insert(Transaction transaction, uint index, string value)
+    {
+        TextChannel.Insert(Handle, transaction.Handle, index, value, nint.Zero);
+    }
+}
---
+++ YDotNet/Native/Types/TextChannel.cs
@@ -0,0 +1,10 @@
+using System.Runtime.InteropServices;
+
+namespace YDotNet.Native.Types;
+
+public static class TextChannel
+{
+    // TODO [LSViana] Add class for `attributes` parameter.
+    [DllImport(ChannelSettings.NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ytext_insert")]
+    public static extern void Insert(nint text, nint transaction, uint index, string value, nint attributes);
+}
