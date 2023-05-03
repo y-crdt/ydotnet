3eaa18f50e417b2bcd68c78b6383612b7cce0a7d
Lucas Viana <lv201122@gmail.com>
2023-05-02 21:57:54 -0300

    test(doc): add tests for ObserveAfterTransaction() and UnobserveAfterTransaction()

---
+++ Tests/YDotNet.Tests.Unit/Document/AfterTransactionTests.cs
@@ -0,0 +1,63 @@
+using NUnit.Framework;
+using YDotNet.Document;
+using YDotNet.Document.Events;
+
+namespace YDotNet.Tests.Unit.Document;
+
+public class AfterTransactionTests
+{
+    [Test]
+    public void TriggersWhenTransactionIsCommittedUntilUnobserve()
+    {
+        // Arrange
+        var doc = new Doc();
+
+        AfterTransactionEvent? afterTransactionEvent = null;
+        var called = 0;
+
+        var text = doc.Text("country");
+        var subscription = doc.ObserveAfterTransaction(
+            e =>
+            {
+                called++;
+                afterTransactionEvent = e;
+            });
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        text.Insert(transaction, index: 0, "Brazil");
+        transaction.Commit();
+
+        // Assert
+        Assert.That(called, Is.EqualTo(expected: 1));
+        Assert.That(afterTransactionEvent, Is.Not.Null);
+        Assert.That(afterTransactionEvent.AfterState, Is.Not.Null);
+        Assert.That(afterTransactionEvent.BeforeState, Is.Not.Null);
+        Assert.That(afterTransactionEvent.DeleteSet, Is.Not.Null);
+
+        // Act
+        afterTransactionEvent = null;
+        transaction = doc.WriteTransaction();
+        text.Insert(transaction, index: 0, "Great ");
+        transaction.Commit();
+
+        // Assert
+        Assert.That(called, Is.EqualTo(expected: 2));
+        Assert.That(afterTransactionEvent, Is.Not.Null);
+        Assert.That(afterTransactionEvent.AfterState, Is.Not.Null);
+        Assert.That(afterTransactionEvent.BeforeState, Is.Not.Null);
+        Assert.That(afterTransactionEvent.DeleteSet, Is.Not.Null);
+
+        // Act
+        afterTransactionEvent = null;
+        doc.UnobserveAfterTransaction(subscription);
+
+        transaction = doc.WriteTransaction();
+        text.Insert(transaction, index: 0, "The ");
+        transaction.Commit();
+
+        // Assert
+        Assert.That(called, Is.EqualTo(expected: 2));
+        Assert.That(afterTransactionEvent, Is.Null);
+    }
+}
