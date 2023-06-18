1339a889537a8131bf47c5fa71f554d7d3267ee3
Lucas Viana <lv201122@gmail.com>
2023-06-18 17:28:49 -0300

    test(transaction): add tests for ApplyV1() on Transaction class

---
+++ Tests/YDotNet.Tests.Unit/Transactions/ApplyV1.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+using YDotNet.Document;
+using YDotNet.Document.Transactions;
+
+namespace YDotNet.Tests.Unit.Transactions;
+
+public class ApplyV1
+{
+    [Test]
+    public void ApplyAndCheckChanges()
+    {
+        // Arrange
+        var senderDoc = ArrangeSenderDoc();
+        var receiverDoc = new Doc();
+        var receiverText = receiverDoc.Text("name");
+        var receiverTransaction = receiverDoc.WriteTransaction();
+        var senderTransaction = senderDoc.ReadTransaction();
+
+        // Act
+        var stateDiff = senderTransaction.StateDiffV1(receiverTransaction.StateVectorV1());
+        var result = receiverTransaction.ApplyV1(stateDiff);
+        var text = receiverText.String(receiverTransaction);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(TransactionUpdateResult.Ok));
+        Assert.That(text, Is.EqualTo("Lucas"));
+    }
+
+    private static Doc ArrangeSenderDoc()
+    {
+        var doc = new Doc();
+        var text = doc.Text("name");
+        var transaction = doc.WriteTransaction();
+
+        text.Insert(transaction, index: 0, "Lucas");
+        transaction.Commit();
+
+        return doc;
+    }
+}
