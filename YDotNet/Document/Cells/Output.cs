0b8b9c01cb7a26c121883245e8977ecabdc59991
Lucas Viana <lv201122@gmail.com>
2023-06-23 22:11:22 -0300

    feat(map): introduce Input cell to read from the storage

---
+++ YDotNet/Document/Cells/Output.cs
@@ -0,0 +1,35 @@
+using YDotNet.Infrastructure;
+using YDotNet.Native.Cells.Outputs;
+
+namespace YDotNet.Document.Cells;
+
+/// <summary>
+///     Represents a cell used to read information from the storage.
+/// </summary>
+public class Output : IDisposable
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="Output" /> class.
+    /// </summary>
+    /// <param name="handle">The pointer to the native resource that represents the storage.</param>
+    internal Output(nint handle)
+    {
+        Handle = handle;
+    }
+
+    /// <summary>
+    ///     Gets the handle to the native resource.
+    /// </summary>
+    internal nint Handle { get; }
+
+    /// <summary>
+    ///     Gets the <see cref="Doc" /> or <c>null</c> if this output cell contains a different type stored.
+    /// </summary>
+    public Doc? Doc => ReferenceAccessor.Access(new Doc(OutputChannel.Doc(Handle)));
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        // TODO [LSViana] Use `youtput_destroy` here.
+    }
+}
