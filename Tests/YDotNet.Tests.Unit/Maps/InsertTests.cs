1f58145ba84894076368be38228353034a7a5bc1
Lucas Viana <lv201122@gmail.com>
2023-06-23 22:44:01 -0300

    test(map): introduce tests to insert documents on Map class (other types are ignored for now)

---
+++ Tests/YDotNet.Tests.Unit/Maps/InsertTests.cs
@@ -0,0 +1,120 @@
+using NUnit.Framework;
+using YDotNet.Document;
+using YDotNet.Document.Cells;
+using YDotNet.Document.Transactions;
+using YDotNet.Document.Types;
+
+namespace YDotNet.Tests.Unit.Maps;
+
+public class InsertTests
+{
+    [Test]
+    [Ignore("To be implemented.")]
+    public void InsertBoolean()
+    {
+    }
+
+    [Test]
+    [Ignore("To be implemented.")]
+    public void InsertDouble()
+    {
+    }
+
+    [Test]
+    [Ignore("To be implemented.")]
+    public void InsertLong()
+    {
+    }
+
+    [Test]
+    [Ignore("To be implemented.")]
+    public void InsertString()
+    {
+    }
+
+    [Test]
+    [Ignore("To be implemented.")]
+    public void InsertBytes()
+    {
+    }
+
+    [Test]
+    [Ignore("To be implemented.")]
+    public void InsertCollection()
+    {
+    }
+
+    [Test]
+    [Ignore("To be implemented.")]
+    public void InsertObject()
+    {
+    }
+
+    [Test]
+    [Ignore("To be implemented.")]
+    public void InsertNull()
+    {
+    }
+
+    [Test]
+    [Ignore("To be implemented.")]
+    public void InsertUndefined()
+    {
+    }
+
+    [Test]
+    [Ignore("To be implemented.")]
+    public void InsertText()
+    {
+    }
+
+    [Test]
+    [Ignore("To be implemented.")]
+    public void InsertArray()
+    {
+    }
+
+    [Test]
+    [Ignore("To be implemented.")]
+    public void InsertMap()
+    {
+    }
+
+    [Test]
+    [Ignore("To be implemented.")]
+    public void InsertXmlElement()
+    {
+    }
+
+    [Test]
+    [Ignore("To be implemented.")]
+    public void InsertXmlText()
+    {
+    }
+
+    [Test]
+    public void InsertDoc()
+    {
+        // Arrange
+        var (map, transaction) = ArrangeDoc();
+        var subDoc = new Doc();
+
+        // Assert
+        Assert.That(map.Length(transaction), Is.EqualTo(expected: 0));
+
+        // Act
+        map.Insert(transaction, "sub-doc", new Input(subDoc));
+
+        // Assert
+        Assert.That(map.Length(transaction), Is.EqualTo(expected: 1));
+    }
+
+    private (Map?, Transaction?) ArrangeDoc()
+    {
+        var doc = new Doc();
+        var map = doc.Map("map");
+        var transaction = doc.WriteTransaction();
+
+        return (map, transaction);
+    }
+}
