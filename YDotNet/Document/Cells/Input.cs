6b3a1051ac23b85e707a03ebe7285c4f7ed286c7
Lucas Viana <lv201122@gmail.com>
2023-06-24 13:59:40 -0300

    feat(map): update Input cell class to have static constructor methods to better define future values like null and undefined

--- YDotNet/Document/Cells/Input.cs
+++ YDotNet/Document/Cells/Input.cs
@@ -6,29 +6,36 @@ namespace YDotNet.Document.Cells;
 /// <summary>
 ///     Represents a cell used to send information to be stored.
 /// </summary>
-public class Input
+public sealed class Input
 {
-    /// <summary>
-    ///     Initializes a new instance of the <see cref="Input" /> class.
-    /// </summary>
-    /// <param name="value">The <see cref="Doc" /> instance to be stored in the cell.</param>
-    public Input(Doc value)
+    private Input(InputNative inputNative)
     {
-        InputNative = InputChannel.Doc(value.Handle);
-    }
-
-    /// <summary>
-    ///     Initializes a new instance of the <see cref="Input" /> class.
-    /// </summary>
-    /// <param name="value">The <see cref="string" /> value to be stored in the cell.</param>
-    public Input(string value)
-    {
-        // TODO [LSViana] Free the memory allocated here.
-        InputNative = InputChannel.String(Marshal.StringToHGlobalAuto(value));
+        InputNative = inputNative;
     }
 
     /// <summary>
     ///     Gets the native input cell represented by this cell.
     /// </summary>
     internal InputNative InputNative { get; }
+
+    /// <summary>
+    ///     Creates a new instance of the <see cref="Input" /> class.
+    /// </summary>
+    /// <param name="value">The <see cref="Doc" /> instance to be stored in the cell.</param>
+    /// <returns>The <see cref="Input" /> cell that represents the provided value.</returns>
+    public static Input Doc(Doc value)
+    {
+        return new Input(InputChannel.Doc(value.Handle));
+    }
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="Input" /> class.
+    /// </summary>
+    /// <param name="value">The <see cref="string" /> value to be stored in the cell.</param>
+    /// <returns>The <see cref="Input" /> cell that represents the provided value.</returns>
+    public static Input String(string value)
+    {
+        // TODO [LSViana] Free the memory allocated here.
+        return new Input(InputChannel.String(Marshal.StringToHGlobalAuto(value)));
+    }
 }
