84ad85b48f1058c02b8ed6bb1ce5fa6753af148a
Lucas Viana <lv201122@gmail.com>
2023-06-25 11:37:38 -0300

    feat(map): implement Bytes() on Input class

--- YDotNet/Document/Cells/Input.cs
+++ YDotNet/Document/Cells/Input.cs
@@ -72,6 +72,16 @@ public sealed class Input
     /// <summary>
     ///     Initializes a new instance of the <see cref="Input" /> class.
     /// </summary>
+    /// <param name="value">The <see cref="long" /> value to be stored in the cell.</param>
+    /// <returns>The <see cref="Input" /> cell that represents the provided value.</returns>
+    public static Input Bytes(byte[] value)
+    {
+        return new Input(InputChannel.Bytes(value, (uint) value.Length));
+    }
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="Input" /> class.
+    /// </summary>
     /// <returns>The <see cref="Input" /> cell that represents the provided value.</returns>
     public static Input Null()
     {
--- YDotNet/Native/Cells/Inputs/InputChannel.cs
+++ YDotNet/Native/Cells/Inputs/InputChannel.cs
@@ -19,6 +19,9 @@ internal static class InputChannel
     [DllImport(ChannelSettings.NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "yinput_long")]
     public static extern InputNative Long(long value);
 
+    [DllImport(ChannelSettings.NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "yinput_binary")]
+    public static extern InputNative Bytes(byte[] value, uint length);
+
     [DllImport(ChannelSettings.NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "yinput_null")]
     public static extern InputNative Null();
 
