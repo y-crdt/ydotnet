eccb62d57e8d5ca5b134d2d58aad63f4b3b833ee
Lucas Viana <lv201122@gmail.com>
2023-07-11 23:36:35 -0300

    test(map): introduce tests to insert Text instances on Map class

--- Tests/YDotNet.Tests.Unit/Maps/InsertTests.cs
+++ Tests/YDotNet.Tests.Unit/Maps/InsertTests.cs
@@ -176,9 +176,19 @@ public class InsertTests
     }
 
     [Test]
-    [Ignore("To be implemented.")]
     public void InsertText()
     {
+        // Arrange
+        var (map, transaction) = ArrangeDoc();
+
+        // Assert
+        Assert.That(map.Length(transaction), Is.EqualTo(expected: 0));
+
+        // Act
+        map.Insert(transaction, "value", Input.Text("Lucas"));
+
+        // Assert
+        Assert.That(map.Length(transaction), Is.EqualTo(expected: 1));
     }
 
     [Test]
