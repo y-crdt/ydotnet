8c2474fc3012c8aa935de5274cb5f7c3327e81ad
Lucas Viana <lv201122@gmail.com>
2023-07-24 22:33:13 -0300

    test(map): introduce tests for Map.Unobserve()

---
+++ Tests/YDotNet.Tests.Unit/Maps/UnobserveTests.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using YDotNet.Document;
+using YDotNet.Document.Cells;
+
+namespace YDotNet.Tests.Unit.Maps;
+
+public class UnobserveTests
+{
+    [Test]
+    public void TriggersWhenMapChangedUntilUnobserved()
+    {
+        // Arrange
+        var doc = new Doc();
+        var map = doc.Map("map");
+        var called = 0;
+        var subscription = map.Observe(_ => called++);
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        map.Insert(transaction, "value1", Input.Long(value: 2469L));
+        transaction.Commit();
+
+        // Assert
+        Assert.That(called, Is.EqualTo(expected: 1));
+
+        // Act
+        map.Unobserve(subscription);
+
+        transaction = doc.WriteTransaction();
+        map.Insert(transaction, "value2", Input.Long(value: -420L));
+        transaction.Commit();
+
+        // Assert
+        Assert.That(called, Is.EqualTo(expected: 1));
+    }
+}
