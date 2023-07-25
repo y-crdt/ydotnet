2646d1cd8532e3cd25064e03d05b89a8d07c08ef
Lucas Viana <lv201122@gmail.com>
2023-07-24 21:14:16 -0300

    test(map): introduce tests for Map.Observe() when updating an existing value

--- Tests/YDotNet.Tests.Unit/Maps/ObserveTests.cs
+++ Tests/YDotNet.Tests.Unit/Maps/ObserveTests.cs
@@ -126,9 +126,45 @@ public class ObserveTests
     }
 
     [Test]
-    [Ignore("To be implemented.")]
     public void ObserveHasKeysWhenUpdated()
     {
+        // Arrange
+        var doc = new Doc();
+        var map = doc.Map("map");
+        var transaction = doc.WriteTransaction();
+        map.Insert(transaction, "value", Input.Long(value: 2469L));
+        transaction.Commit();
+
+        IEnumerable<MapEventKeyChange>? keyChanges = null;
+        var called = 0;
+
+        var subscription = map.Observe(
+            e =>
+            {
+                called++;
+                keyChanges = e.Keys.ToArray();
+            });
+
+        // Act
+        transaction = doc.WriteTransaction();
+        map.Insert(transaction, "value", Input.Long(value: -420L));
+        transaction.Commit();
+
+        // Assert
+        var firstKey = keyChanges.First();
+
+        Assert.That(called, Is.EqualTo(expected: 1));
+        Assert.That(subscription.Id, Is.EqualTo(expected: 0L));
+        Assert.That(keyChanges, Is.Not.Null);
+        Assert.That(keyChanges.Count(), Is.EqualTo(expected: 1));
+        Assert.That(firstKey.Key, Is.EqualTo("value"));
+        Assert.That(firstKey.Tag, Is.EqualTo(MapEventKeyChangeTag.Update));
+        Assert.That(firstKey.OldValue, Is.Not.Null);
+        Assert.That(firstKey.NewValue, Is.Not.Null);
+        Assert.That(firstKey.OldValue.Long, Is.EqualTo(expected: 2469L));
+        Assert.That(firstKey.OldValue.Double, Is.Null);
+        Assert.That(firstKey.NewValue.Long, Is.EqualTo(expected: -420L));
+        Assert.That(firstKey.NewValue.Double, Is.Null);
     }
 
     [Test]
