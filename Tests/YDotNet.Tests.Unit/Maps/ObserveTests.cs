05cd42618a60e207d1672ceeea18ad581331a9c9
Lucas Viana <lv201122@gmail.com>
2023-07-24 21:22:20 -0300

    test(map): introduce tests for Map.Observe() when adding, removing, and updating in a single transaction

--- Tests/YDotNet.Tests.Unit/Maps/ObserveTests.cs
+++ Tests/YDotNet.Tests.Unit/Maps/ObserveTests.cs
@@ -168,9 +168,65 @@ public class ObserveTests
     }
 
     [Test]
-    [Ignore("To be implemented.")]
     public void ObserveHasKeysWhenAddedAndRemovedAndUpdated()
     {
+        // Arrange
+        var doc = new Doc();
+        var map = doc.Map("map");
+        var transaction = doc.WriteTransaction();
+        map.Insert(transaction, "value1", Input.Long(value: 2469L));
+        map.Insert(transaction, "value2", Input.Long(value: -420L));
+        transaction.Commit();
+
+        IEnumerable<MapEventKeyChange>? keyChanges = null;
+        var called = 0;
+
+        var subscription = map.Observe(
+            e =>
+            {
+                called++;
+                keyChanges = e.Keys.ToArray();
+            });
+
+        // Act
+        transaction = doc.WriteTransaction();
+
+        // Update, remove, and add, respectively
+        map.Insert(transaction, "value1", Input.Long(value: -420L));
+        map.Remove(transaction, "value2");
+        map.Insert(transaction, "value3", Input.Long(value: -1337L));
+
+        transaction.Commit();
+
+        // Assert
+
+        Assert.That(called, Is.EqualTo(expected: 1));
+        Assert.That(subscription.Id, Is.EqualTo(expected: 0L));
+        Assert.That(keyChanges, Is.Not.Null);
+        Assert.That(keyChanges.Count(), Is.EqualTo(expected: 3));
+        Assert.That(keyChanges.Count(x => x.Tag == MapEventKeyChangeTag.Update), Is.EqualTo(expected: 1));
+        Assert.That(keyChanges.Count(x => x.Tag == MapEventKeyChangeTag.Remove), Is.EqualTo(expected: 1));
+        Assert.That(keyChanges.Count(x => x.Tag == MapEventKeyChangeTag.Add), Is.EqualTo(expected: 1));
+
+        var update = keyChanges.First(x => x.Tag == MapEventKeyChangeTag.Update);
+        Assert.That(update.OldValue, Is.Not.Null);
+        Assert.That(update.NewValue, Is.Not.Null);
+        Assert.That(update.OldValue.Long, Is.EqualTo(expected: 2469L));
+        Assert.That(update.OldValue.Double, Is.Null);
+        Assert.That(update.NewValue.Long, Is.EqualTo(expected: -420L));
+        Assert.That(update.NewValue.Double, Is.Null);
+
+        var remove = keyChanges.First(x => x.Tag == MapEventKeyChangeTag.Remove);
+        Assert.That(remove.OldValue, Is.Not.Null);
+        Assert.That(remove.NewValue, Is.Null);
+        Assert.That(remove.OldValue.Long, Is.EqualTo(expected: -420L));
+        Assert.That(remove.OldValue.Double, Is.Null);
+
+        var add = keyChanges.First(x => x.Tag == MapEventKeyChangeTag.Add);
+        Assert.That(add.OldValue, Is.Null);
+        Assert.That(add.NewValue, Is.Not.Null);
+        Assert.That(add.NewValue.Long, Is.EqualTo(expected: -1337L));
+        Assert.That(add.NewValue.Double, Is.Null);
     }
 
     [Test]
