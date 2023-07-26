38fd8dc775f4fff51711a4bf00f221027e0938c2
Lucas Viana <lv201122@gmail.com>
2023-07-25 23:41:07 -0300

    refactor(map): introduce the EventKeys class to hold the state and dispose resources related to keys
    This is also useful to implement the other `*_event_keys()` events later, since they all share the same signature on the C FFI.

---
+++ YDotNet/Document/Types/Events/EventKeys.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Runtime.InteropServices;
+using YDotNet.Infrastructure;
+using YDotNet.Native.Types;
+using YDotNet.Native.Types.Events;
+
+namespace YDotNet.Document.Types.Events;
+
+/// <summary>
+///     Represents the keys that changed in an event that happened in the parent instance.
+/// </summary>
+public class EventKeys : IEnumerable<EventKeyChange>, IDisposable
+{
+    private readonly IEnumerable<EventKeyChange> collection;
+
+    internal EventKeys(nint handle, uint length)
+    {
+        Handle = handle;
+        Length = length;
+
+        collection = MemoryReader.TryReadIntPtrArray(Handle, Length, Marshal.SizeOf<EventKeyChangeNative>())
+            ?.Select(Marshal.PtrToStructure<EventKeyChangeNative>)
+            .Select(x => x.ToMapEventKeyChange())
+            .ToArray();
+    }
+
+    /// <summary>
+    ///     Gets the handle to the native resource.
+    /// </summary>
+    public uint Length { get; }
+
+    /// <summary>
+    ///     Gets the handle to the native resource.
+    /// </summary>
+    internal nint Handle { get; }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        EventChannel.Destroy(Handle, Length);
+    }
+
+    /// <inheritdoc />
+    public IEnumerator<EventKeyChange> GetEnumerator()
+    {
+        return collection.GetEnumerator();
+    }
+
+    /// <inheritdoc />
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
