00b0850251514fa213d81cbcb29563b6946b26cd
Lucas Viana <lv201122@gmail.com>
2023-07-27 19:10:15 -0300

    docs(map): fix documentation and access modifiers for EventKeys

--- YDotNet/Document/Types/Events/EventKeys.cs
+++ YDotNet/Document/Types/Events/EventKeys.cs
@@ -7,27 +7,33 @@ using YDotNet.Native.Types.Events;
 namespace YDotNet.Document.Types.Events;
 
 /// <summary>
-///     Represents the keys that changed in an event that happened in the parent instance.
+///     Represents the keys that changed the shared type that emitted the event related to this <see cref="EventKeys" />
+///     instance.
 /// </summary>
 public class EventKeys : IEnumerable<EventKeyChange>, IDisposable
 {
     private readonly IEnumerable<EventKeyChange> collection;
 
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="EventKeys" /> class.
+    /// </summary>
+    /// <param name="handle">The handle to the beginning of the array of <see cref="EventKeyChangeNative" /> instances.</param>
+    /// <param name="length">The length of the array.</param>
     internal EventKeys(nint handle, uint length)
     {
         Handle = handle;
         Length = length;
 
-        collection = MemoryReader.TryReadIntPtrArray(Handle, Length, Marshal.SizeOf<EventKeyChangeNative>())
-            ?.Select(Marshal.PtrToStructure<EventKeyChangeNative>)
+        collection = MemoryReader.TryReadIntPtrArray(Handle, Length, Marshal.SizeOf<EventKeyChangeNative>())!
+            .Select(Marshal.PtrToStructure<EventKeyChangeNative>)
             .Select(x => x.ToEventKeyChange())
             .ToArray();
     }
 
     /// <summary>
-    ///     Gets the handle to the native resource.
+    ///     Gets the length of the native resource.
     /// </summary>
-    public uint Length { get; }
+    internal uint Length { get; }
 
     /// <summary>
     ///     Gets the handle to the native resource.
