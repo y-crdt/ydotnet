29d4f4f0dc8f2a776a7fe0b2e12b531f72e86b53
Lucas Viana <lv201122@gmail.com>
2023-07-27 19:28:39 -0300

    test(map): introduce tests for MapEvent.Path when removing all items

--- Tests/YDotNet.Tests.Unit/Maps/ObserveDeepTests.cs
+++ Tests/YDotNet.Tests.Unit/Maps/ObserveDeepTests.cs
@@ -91,9 +91,46 @@ public class ObserveDeepTests
     }
 
     [Test]
-    [Ignore("To be implemented.")]
     public void ObserveHasPathWhenRemovedAll()
     {
+        // Arrange
+        var (doc, map1, _, map3) = ArrangeDoc();
+
+        var transaction = doc.WriteTransaction();
+        map3.Insert(transaction, "value", Input.Long(value: 2469L));
+        transaction.Commit();
+
+        IEnumerable<EventPathSegment>? pathSegments = null;
+        var called = 0;
+
+        var subscription = map1.ObserveDeep(
+            events =>
+            {
+                called++;
+                pathSegments = events.First().MapEvent.Path.ToArray();
+            });
+
+        // Act
+        transaction = doc.WriteTransaction();
+        map3.RemoveAll(transaction);
+        transaction.Commit();
+
+        // Assert
+        Assert.That(called, Is.EqualTo(expected: 1));
+        Assert.That(subscription.Id, Is.EqualTo(expected: 0L));
+        Assert.That(pathSegments, Is.Not.Null);
+        Assert.That(pathSegments.Count(), Is.EqualTo(expected: 2));
+
+        var first = pathSegments.ElementAt(index: 0);
+        var second = pathSegments.ElementAt(index: 1);
+
+        Assert.That(first.Tag, Is.EqualTo(EventPathSegmentTag.Key));
+        Assert.That(first.Key, Is.EqualTo("map-2"));
+        Assert.That(first.Index, Is.Null);
+
+        Assert.That(second.Tag, Is.EqualTo(EventPathSegmentTag.Key));
+        Assert.That(second.Key, Is.EqualTo("map-3"));
+        Assert.That(second.Index, Is.Null);
     }
 
     [Test]
