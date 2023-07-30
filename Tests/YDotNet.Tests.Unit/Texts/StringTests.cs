ee587456de477f34d738b961ce319d54cd9ff291
Lucas Viana <lv201122@gmail.com>
2023-07-29 21:35:38 -0300

    test(text): introduce tests for Text.String()

---
+++ Tests/YDotNet.Tests.Unit/Texts/StringTests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using YDotNet.Document;
+
+namespace YDotNet.Tests.Unit.Texts;
+
+public class StringTests
+{
+    [Test]
+    public void ReturnsTheFullText()
+    {
+        // Arrange
+        var doc = new Doc();
+        var text = doc.Text("name");
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        text.Insert(transaction, index: 0, "Star. ⭐");
+        transaction.Commit();
+
+        // Assert
+        Assert.That(text.String(transaction), Is.EqualTo("Star. ⭐"));
+    }
+}
