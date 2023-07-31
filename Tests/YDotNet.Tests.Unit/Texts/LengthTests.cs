340b09d7e476ec06a2300e89f36c7a3bf49defdf
Lucas Viana <lv201122@gmail.com>
2023-07-30 21:06:36 -0300

    test(text): introduce tests for Text.Length()

---
+++ Tests/YDotNet.Tests.Unit/Texts/LengthTests.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+using YDotNet.Document;
+
+namespace YDotNet.Tests.Unit.Texts;
+
+public class LengthTests
+{
+    [Test]
+    public void LengthIsInitiallyZero()
+    {
+        // Arrange
+        var doc = new Doc();
+        var text = doc.Text("name");
+
+        // Act
+        var transaction = doc.ReadTransaction();
+        var length = text.Length(transaction);
+
+        // Assert
+        Assert.That(length, Is.EqualTo(expected: 0));
+    }
+
+    [Test]
+    public void LengthIsCorrectForAlphanumericCharacters()
+    {
+        // Arrange
+        var doc = new Doc();
+        var text = doc.Text("name");
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        text.Insert(transaction, index: 0, "Lucas");
+        var length = text.Length(transaction);
+
+        // Assert
+        Assert.That(length, Is.EqualTo(expected: 5));
+    }
+
+    [Test]
+    public void LengthIsCorrectForAlphanumericAndEmojiCharacters()
+    {
+        // Arrange
+        var doc = new Doc();
+        var text = doc.Text("name");
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        text.Insert(transaction, index: 0, "Earth 🌎");
+        var length = text.Length(transaction);
+
+        // Assert
+        Assert.That(length, Is.EqualTo(expected: 10));
+    }
+}
