ad9ca6b1f473e1c4be92ed90bb224d61ec5a9282
Lucas Viana <lv201122@gmail.com>
2023-07-30 21:32:11 -0300

    feat(text): introduce tests for Text.Insert() with attributes

--- Tests/YDotNet.Tests.Unit/Texts/InsertTests.cs
+++ Tests/YDotNet.Tests.Unit/Texts/InsertTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using YDotNet.Document;
+using YDotNet.Document.Cells;
 
 namespace YDotNet.Tests.Unit.Texts;
 
@@ -49,6 +50,68 @@ public class InsertTests
         var transaction = doc.WriteTransaction();
         text.Insert(transaction, index: 0, "Lcas");
         text.Insert(transaction, index: 1, "u");
+        transaction.Commit();
+
+        // Assert
+        Assert.That(text.String(transaction), Is.EqualTo("Lucas"));
+    }
+
+    [Test]
+    public void AddsTextAtBeginningWithAttributes()
+    {
+        // Arrange
+        var doc = new Doc();
+        var text = doc.Text("name");
+        var attributes = new Dictionary<string, Input>
+        {
+            { "bold", Input.Boolean(value: true) }
+        };
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        text.Insert(transaction, index: 0, "Hello", Input.Object(attributes));
+        transaction.Commit();
+
+        // Assert
+        Assert.That(text.String(transaction), Is.EqualTo("Hello"));
+    }
+
+    [Test]
+    public void AddsTextAtEndingWithAttributes()
+    {
+        // Arrange
+        var doc = new Doc();
+        var text = doc.Text("name");
+        var attributes = new Dictionary<string, Input>
+        {
+            { "bold", Input.Boolean(value: true) }
+        };
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        text.Insert(transaction, index: 0, "Hello");
+        text.Insert(transaction, index: 5, ", world!", Input.Object(attributes));
+        transaction.Commit();
+
+        // Assert
+        Assert.That(text.String(transaction), Is.EqualTo("Hello, world!"));
+    }
+
+    [Test]
+    public void AddsTextAtMiddleWithAttributes()
+    {
+        // Arrange
+        var doc = new Doc();
+        var text = doc.Text("name");
+        var attributes = new Dictionary<string, Input>
+        {
+            { "bold", Input.Boolean(value: true) }
+        };
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        text.Insert(transaction, index: 0, "Lcas");
+        text.Insert(transaction, index: 1, "u", Input.Object(attributes));
         transaction.Commit();
 
         // Assert
