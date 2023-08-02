f1d87f45a3a1ba51eb7b56bddefb75dba4917255
Lucas Viana <lv201122@gmail.com>
2023-08-01 22:44:33 -0300

    refactor(text): remove unnecessary TODO comment that is already implemented

--- YDotNet/Native/Types/TextChannel.cs
+++ YDotNet/Native/Types/TextChannel.cs
@@ -4,7 +4,6 @@ namespace YDotNet.Native.Types;
 
 internal static class TextChannel
 {
-    // TODO [LSViana] Add class for `attributes` parameter.
     [DllImport(ChannelSettings.NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ytext_insert")]
     public static extern void Insert(nint text, nint transaction, uint index, string value, nint attributes);
 
