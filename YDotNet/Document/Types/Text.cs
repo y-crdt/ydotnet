5d106b522671f9cdc9ecf54eb4de036686e58506
Lucas Viana <lv201122@gmail.com>
2023-08-01 22:54:12 -0300

    feat(text): implement Format() on Text class

--- YDotNet/Document/Types/Text.cs
+++ YDotNet/Document/Types/Text.cs
@@ -43,6 +43,26 @@ public class Text
     }
 
     /// <summary>
+    ///     Formats a string in the given range defined by <c>index</c> and <c>length</c>.
+    /// </summary>
+    /// <param name="transaction">The transaction that wraps this write operation.</param>
+    /// <param name="index">The index must be between 0 and <see cref="Length" /> or an exception will be thrown.</param>
+    /// <param name="length">
+    ///     The length of the text to be formatted, relative to the index and must not go over total
+    ///     <see cref="Length" />.
+    /// </param>
+    /// <param name="attributes">
+    ///     Optional, the attributes to be added to the inserted text. The value must be the result of a call to
+    ///     <see cref="Input" />.<see cref="Input.Object" />.
+    /// </param>
+    public void Format(Transaction transaction, uint index, uint length, Input attributes)
+    {
+        var attributesPointer = MemoryWriter.WriteStruct(attributes.InputNative);
+        TextChannel.Format(Handle, transaction.Handle, index, length, attributesPointer);
+        MemoryWriter.Release(attributesPointer);
+    }
+
+    /// <summary>
     ///     Returns the full string stored in the instance.
     /// </summary>
     /// <param name="transaction">The transaction that wraps this read operation.</param>
--- YDotNet/Native/Types/TextChannel.cs
+++ YDotNet/Native/Types/TextChannel.cs
@@ -7,6 +7,9 @@ internal static class TextChannel
     [DllImport(ChannelSettings.NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ytext_insert")]
     public static extern void Insert(nint text, nint transaction, uint index, string value, nint attributes);
 
+    [DllImport(ChannelSettings.NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ytext_format")]
+    public static extern void Format(nint text, nint transaction, uint index, uint length, nint attributes);
+
     // TODO [LSViana] Check if the transaction reference is really needed here.
     [DllImport(ChannelSettings.NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ytext_string")]
     public static extern nint String(nint text, nint transaction);
