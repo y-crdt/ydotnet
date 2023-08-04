fc519c48fa43340a28ad185f2533d185011eef13
Lucas Viana <lv201122@gmail.com>
2023-08-03 22:39:00 -0300

    test(text): fix tests for Text.UnobserveDeep()

--- Tests/YDotNet.Tests.Unit/Texts/UnobserveDeepTests.cs
+++ Tests/YDotNet.Tests.Unit/Texts/UnobserveDeepTests.cs
@@ -10,7 +10,7 @@ public class UnobserveDeepTests
     {
         // Arrange
         var doc = new Doc();
-        var text = doc.Text("map");
+        var text = doc.Text("text");
         var called = 0;
         var subscription = text.ObserveDeep(_ => called++);
 
