47ffd85937a634642dcd75ed449dca5f5f953fad
Lucas Viana <lv201122@gmail.com>
2023-08-03 22:40:25 -0300

    test(text): introduce tests for Text.Unobserve()

---
+++ Tests/YDotNet.Tests.Unit/Texts/UnobserveTests.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using YDotNet.Document;
+
+namespace YDotNet.Tests.Unit.Texts;
+
+public class UnobserveTests
+{
+    [Test]
+    public void TriggersWhenTextChangedUntilUnobserved()
+    {
+        // Arrange
+        var doc = new Doc();
+        var text = doc.Text("value");
+        var called = 0;
+        var subscription = text.Observe(_ => called++);
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        text.Insert(transaction, index: 0, "World");
+        transaction.Commit();
+
+        // Assert
+        Assert.That(called, Is.EqualTo(expected: 1));
+
+        // Act
+        text.Unobserve(subscription);
+
+        transaction = doc.WriteTransaction();
+        text.Insert(transaction, index: 0, "Hello, ");
+        transaction.Commit();
+
+        // Assert
+        Assert.That(called, Is.EqualTo(expected: 1));
+    }
+}
