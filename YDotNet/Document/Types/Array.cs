eb8a5f3e8b886ee2acb348dee208f2f5b8d791a8
Lucas Viana <lv201122@gmail.com>
2023-08-03 21:46:22 -0300

    refactor(text): extend Branch from Array, Map, XmlElement, and XmlText

--- YDotNet/Document/Types/Array.cs
+++ YDotNet/Document/Types/Array.cs
@@ -1,3 +1,4 @@
+using YDotNet.Document.Types.Branches;
 using YDotNet.Native.Types;
 
 namespace YDotNet.Document.Types;
@@ -5,24 +6,20 @@ namespace YDotNet.Document.Types;
 /// <summary>
 ///     A shared data type that represents an array.
 /// </summary>
-public class Array
+public class Array : Branch
 {
     /// <summary>
     ///     Initializes a new instance of the <see cref="Array" /> class.
     /// </summary>
     /// <param name="handle">The handle to the native resource.</param>
     internal Array(nint handle)
+        : base(handle)
     {
-        Handle = handle;
+        // Nothing here.
     }
 
     /// <summary>
     ///     Gets the number of elements stored within current instance of <see cref="Types.Array" />.
     /// </summary>
     public uint Length => ArrayChannel.Length(Handle);
-
-    /// <summary>
-    ///     Gets the handle to the native resource.
-    /// </summary>
-    internal nint Handle { get; }
 }
