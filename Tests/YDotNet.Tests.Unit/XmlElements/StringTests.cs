4e67661ae03683489b58b5bc1c57e3349a440485
Lucas Viana <lv201122@gmail.com>
2023-08-09 23:27:37 -0300

    test(xml-element): introduce tests for XmlElement.String and ToString() override

---
+++ Tests/YDotNet.Tests.Unit/XmlElements/StringTests.cs
@@ -0,0 +1,103 @@
+using NUnit.Framework;
+using YDotNet.Document;
+using YDotNet.Document.Cells;
+
+namespace YDotNet.Tests.Unit.XmlElements;
+
+public class StringTests
+{
+    [Test]
+    [Ignore("Check the Tag implementation for root-level nodes.")]
+    public void RootLevelNodeIsEmptyByDefault()
+    {
+        // Arrange
+        var doc = new Doc();
+        var xmlElement = doc.XmlElement("xml-element");
+
+        // Act
+        var text = xmlElement.String;
+
+        // Assert
+        Assert.That(text, Is.Null);
+    }
+
+    [Test]
+    [Ignore("Waiting for the implementation of XmlElement.InsertText().")]
+    public void RootLevelNodeHasCorrectStringWithoutChildren()
+    {
+        // TODO [LSViana] Implement this test when XmlElement.InsertText() is available.
+    }
+
+    [Test]
+    [Ignore("Waiting for the implementation of XmlElement.InsertText().")]
+    public void RootLevelNodeHasCorrectStringWithChildren()
+    {
+        // TODO [LSViana] Implement this test when XmlElement.InsertText() is available.
+    }
+
+    [Test]
+    public void NodeNestedOnMapHasCorrectString()
+    {
+        // TODO [LSViana] Implement checks for content and attribute when XmlElement.InsertText() is available.
+
+        // Arrange
+        var doc = new Doc();
+        var map = doc.Map("map");
+
+        var transaction = doc.WriteTransaction();
+        map.Insert(transaction, "xml-element", Input.XmlElement("color"));
+        transaction.Commit();
+
+        // Act
+        transaction = doc.ReadTransaction();
+        var text = map.Get(transaction, "xml-element").XmlElement.String;
+        transaction.Commit();
+
+        // Assert
+        Assert.That(text, Is.EqualTo("<color></color>"));
+    }
+
+    [Test]
+    public void NodeNestedOnArrayHasCorrectString()
+    {
+        // TODO [LSViana] Implement checks for content and attribute when XmlElement.InsertText() is available.
+
+        // Arrange
+        var doc = new Doc();
+        var array = doc.Array("array");
+
+        var transaction = doc.WriteTransaction();
+        array.InsertRange(transaction, index: 0, new[] { Input.XmlElement("color") });
+        transaction.Commit();
+
+        // Act
+        transaction = doc.ReadTransaction();
+        var tag = array.Get(transaction, index: 0).XmlElement.String;
+        transaction.Commit();
+
+        // Assert
+        Assert.That(tag, Is.EqualTo("color"));
+    }
+
+    [Test]
+    public void ToStringIsOverriddenToInvokeString()
+    {
+        // TODO [LSViana] Implement checks for content and attribute when XmlElement.InsertText() is available.
+
+        // Arrange
+        var doc = new Doc();
+        var map = doc.Map("map");
+
+        var transaction = doc.WriteTransaction();
+        map.Insert(transaction, "xml-element", Input.XmlElement("color"));
+        transaction.Commit();
+
+        // Act
+        transaction = doc.ReadTransaction();
+        var text = map.Get(transaction, "xml-element").XmlElement.ToString();
+        transaction.Commit();
+
+        // Assert
+        Assert.That(text, Is.EqualTo("<color></color>"));
+    }
+}
