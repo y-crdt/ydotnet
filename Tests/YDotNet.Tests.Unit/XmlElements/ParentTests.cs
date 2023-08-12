6dc54206f346bb2724dd845eb541766a32d60f27
Lucas Viana <lv201122@gmail.com>
2023-08-12 11:50:08 -0300

    test(xml-element): introduce tests for XmlElement.Parent()

---
+++ Tests/YDotNet.Tests.Unit/XmlElements/ParentTests.cs
@@ -0,0 +1,121 @@
+using NUnit.Framework;
+using YDotNet.Document;
+using YDotNet.Document.Cells;
+
+namespace YDotNet.Tests.Unit.XmlElements;
+
+public class ParentTests
+{
+    [Test]
+    public void ParentOfRootNodeReturnsNull()
+    {
+        // Arrange
+        var doc = new Doc();
+        var xmlElement = doc.XmlElement("xml-element");
+
+        // Act
+        var transaction = doc.ReadTransaction();
+        var parentXmlElement = xmlElement.Parent(transaction);
+        transaction.Commit();
+
+        // Assert
+        Assert.That(parentXmlElement, Is.Null);
+    }
+
+    [Test]
+    public void ParentOfNodeInsideArrayReturnsNull()
+    {
+        // Arrange
+        var doc = new Doc();
+        var array = doc.Array("array");
+
+        var transaction = doc.WriteTransaction();
+        array.InsertRange(transaction, index: 0, new[] { Input.XmlElement("attributes") });
+        transaction.Commit();
+
+        // Act
+        transaction = doc.ReadTransaction();
+        var xmlElement = array.Get(transaction, index: 0).XmlElement;
+        var parentXmlElement = xmlElement.Parent(transaction);
+        transaction.Commit();
+
+        // Assert
+        Assert.That(parentXmlElement, Is.Null);
+    }
+
+    [Test]
+    public void ParentOfNodeInsideMapReturnsNull()
+    {
+        // Arrange
+        var doc = new Doc();
+        var map = doc.Map("array");
+
+        var transaction = doc.WriteTransaction();
+        map.Insert(transaction, "attributes", Input.XmlElement("attributes"));
+        transaction.Commit();
+
+        // Act
+        transaction = doc.ReadTransaction();
+        var xmlElement = map.Get(transaction, "attributes").XmlElement;
+        var parentXmlElement = xmlElement.Parent(transaction);
+        transaction.Commit();
+
+        // Assert
+        Assert.That(parentXmlElement, Is.Null);
+    }
+
+    [Test]
+    public void ParentOfNestedNodeAtFirstLevelReturnsCorrectParent()
+    {
+        // Arrange
+        var doc = new Doc();
+        var xmlElement = doc.XmlElement("xml-element");
+
+        var transaction = doc.WriteTransaction();
+        xmlElement.InsertElement(transaction, index: 0, "width");
+        xmlElement.InsertElement(transaction, index: 1, "color");
+        transaction.Commit();
+
+        // Act
+        transaction = doc.ReadTransaction();
+        var childXmlElement = xmlElement.Get(transaction, index: 0).XmlElement;
+
+        // TODO [LSViana] Check with the team why this is returning `null`.
+        var parentXmlElement = childXmlElement.Parent(transaction);
+        var childLength = parentXmlElement.ChildLength(transaction);
+        transaction.Commit();
+
+        // Assert
+        Assert.That(parentXmlElement, Is.Not.Null);
+        Assert.That(childLength, Is.EqualTo(expected: 2));
+    }
+
+    [Test]
+    public void ParentOfNestedNodeAtSecondLevelReturnsCorrectParent()
+    {
+        // Arrange
+        var doc = new Doc();
+        var xmlElement = doc.XmlElement("xml-element");
+
+        var transaction = doc.WriteTransaction();
+        xmlElement.InsertElement(transaction, index: 0, "dimensions");
+        var childXmlElement = xmlElement.Get(transaction, index: 0).XmlElement;
+        childXmlElement.InsertElement(transaction, index: 0, "width");
+        childXmlElement.InsertElement(transaction, index: 0, "height");
+        childXmlElement.InsertElement(transaction, index: 0, "depth");
+        transaction.Commit();
+
+        // Act
+        transaction = doc.ReadTransaction();
+        var grandChildXmlElement = xmlElement.Get(transaction, index: 0).XmlElement;
+
+        // TODO [LSViana] Check with the team why this is returning `null`.
+        var parentXmlElement = grandChildXmlElement.Parent(transaction);
+        var childLength = parentXmlElement.ChildLength(transaction);
+        transaction.Commit();
+
+        // Assert
+        Assert.That(parentXmlElement, Is.Not.Null);
+        Assert.That(childLength, Is.EqualTo(expected: 2));
+    }
+}
