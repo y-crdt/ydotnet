d23a56505ceafc9d4dfd370c5bd714d23a5aecae
Lucas Viana <lv201122@gmail.com>
2023-08-12 01:16:48 -0300

    test(xml-element): introduce tests for XmlElement.Get()

---
+++ Tests/YDotNet.Tests.Unit/XmlElements/GetTests.cs
@@ -0,0 +1,72 @@
+using NUnit.Framework;
+using YDotNet.Document;
+using YDotNet.Document.Types.XmlElements;
+
+namespace YDotNet.Tests.Unit.XmlElements;
+
+public class GetTests
+{
+    [Test]
+    public void GetOutsideOfValidBounds()
+    {
+        // Arrange
+        var doc = new Doc();
+        var xmlElement = doc.XmlElement("xml-element");
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        var value = xmlElement.Get(transaction, index: 1);
+        transaction.Commit();
+
+        // Assert
+        Assert.That(value, Is.Null);
+    }
+
+    [Test]
+    public void GetXmlText()
+    {
+        // Arrange
+        var (doc, xmlElement) = ArrangeDoc();
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        var output = xmlElement.Get(transaction, index: 1);
+        transaction.Commit();
+
+        // Assert
+        Assert.That(output.XmlElement, Is.Null);
+        Assert.That(output.XmlText, Is.Not.Null);
+    }
+
+    [Test]
+    public void GetXmlElement()
+    {
+        // Arrange
+        var (doc, xmlElement) = ArrangeDoc();
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        var output = xmlElement.Get(transaction, index: 2);
+        transaction.Commit();
+
+        // Assert
+        Assert.That(output.XmlElement, Is.Not.Null);
+        Assert.That(output.XmlText, Is.Null);
+    }
+
+    private (Doc, XmlElement) ArrangeDoc()
+    {
+        var doc = new Doc();
+        var xmlElement = doc.XmlElement("xml-element");
+
+        var transaction = doc.WriteTransaction();
+        xmlElement.InsertText(transaction, index: 0);
+        xmlElement.InsertText(transaction, index: 1);
+        xmlElement.InsertElement(transaction, index: 2, "color");
+        xmlElement.InsertText(transaction, index: 3);
+        xmlElement.InsertElement(transaction, index: 4, "border");
+        transaction.Commit();
+
+        return (doc, xmlElement);
+    }
+}
