6d3c5cd43e10286147523853f7267fd46c47d3eb
Lucas Viana <lv201122@gmail.com>
2023-08-20 13:47:26 -0300

    test(xml-text): introduce tests for XmlTextEvent.Keys

--- Tests/YDotNet.Tests.Unit/XmlTexts/ObserveTests.cs
+++ Tests/YDotNet.Tests.Unit/XmlTexts/ObserveTests.cs
@@ -97,4 +97,104 @@ public class ObserveTests
         Assert.That(deltas, Is.Not.Null);
         Assert.That(deltas, Is.Empty);
     }
+
+    [Test]
+    public void ObserveHasKeysWhenAddedAttributes()
+    {
+        // Arrange
+        var doc = new Doc();
+        var xmlText = doc.XmlText("xml-text");
+
+        IEnumerable<EventKeyChange>? deltas = null;
+        xmlText.Observe(e => deltas = e.Keys.ToArray());
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        xmlText.InsertAttribute(transaction, "color", "red");
+        xmlText.InsertAttribute(transaction, "width", "16");
+        transaction.Commit();
+
+        // Assert
+        Assert.That(deltas, Is.Not.Null);
+        Assert.That(deltas.Count(), Is.EqualTo(expected: 2));
+        Assert.That(deltas.All(x => x.Tag == EventKeyChangeTag.Add), Is.True);
+        Assert.That(deltas.All(x => x.OldValue == null), Is.True);
+        Assert.That(deltas.First(x => x.Key == "color").NewValue.String, Is.EqualTo("red"));
+        Assert.That(deltas.First(x => x.Key == "width").NewValue.String, Is.EqualTo("16"));
+    }
+
+    [Test]
+    public void ObserveHasKeysWhenUpdatedAttributes()
+    {
+        // Arrange
+        var doc = new Doc();
+        var xmlText = doc.XmlText("xml-text");
+
+        var transaction = doc.WriteTransaction();
+        xmlText.InsertAttribute(transaction, "color", "red");
+        transaction.Commit();
+
+        IEnumerable<EventKeyChange>? keyChanges = null;
+        xmlText.Observe(e => keyChanges = e.Keys.ToArray());
+
+        // Act
+        transaction = doc.WriteTransaction();
+        xmlText.InsertAttribute(transaction, "color", "blue");
+        transaction.Commit();
+
+        // Assert
+        Assert.That(keyChanges, Is.Not.Null);
+        Assert.That(keyChanges.Count(), Is.EqualTo(expected: 1));
+        Assert.That(keyChanges.First().Tag, Is.EqualTo(EventKeyChangeTag.Update));
+        Assert.That(keyChanges.First().NewValue.String, Is.EqualTo("blue"));
+        Assert.That(keyChanges.First().OldValue.String, Is.EqualTo("red"));
+    }
+
+    [Test]
+    public void ObserveHasKeysWhenRemovedAttributes()
+    {
+        // Arrange
+        var doc = new Doc();
+        var xmlText = doc.XmlText("xml-text");
+
+        var transaction = doc.WriteTransaction();
+        xmlText.InsertAttribute(transaction, "color", "red");
+        transaction.Commit();
+
+        IEnumerable<EventKeyChange>? keyChanges = null;
+        xmlText.Observe(e => keyChanges = e.Keys.ToArray());
+
+        // Act
+        transaction = doc.WriteTransaction();
+        xmlText.RemoveAttribute(transaction, "color");
+        transaction.Commit();
+
+        // Assert
+        Assert.That(keyChanges, Is.Not.Null);
+        Assert.That(keyChanges.Count(), Is.EqualTo(expected: 1));
+        Assert.That(keyChanges.First().Tag, Is.EqualTo(EventKeyChangeTag.Remove));
+        Assert.That(keyChanges.First().OldValue.String, Is.EqualTo("red"));
+        Assert.That(keyChanges.First().NewValue, Is.Null);
+    }
+
+    [Test]
+    public void ObserveDoesNotHaveKeysWhenAddedTextsAndEmbeds()
+    {
+        // Arrange
+        var doc = new Doc();
+        var xmlText = doc.XmlText("xml-text");
+
+        IEnumerable<EventKeyChange>? keyChanges = null;
+        xmlText.Observe(e => keyChanges = e.Keys.ToArray());
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        xmlText.Insert(transaction, index: 0, "Lucas");
+        xmlText.InsertEmbed(transaction, index: 3, Input.Boolean(value: true));
+        transaction.Commit();
+
+        // Assert
+        Assert.That(keyChanges, Is.Not.Null);
+        Assert.That(keyChanges, Is.Empty);
+    }
 }
