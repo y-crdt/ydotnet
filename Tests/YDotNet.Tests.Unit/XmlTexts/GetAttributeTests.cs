1eded03d1adff629c027fe032ff24c1dffc9ebd2
Lucas Viana <lv201122@gmail.com>
2023-08-20 11:16:52 -0300

    test(xml-text): introduce tests for XmlText.GetAttribute()

---
+++ Tests/YDotNet.Tests.Unit/XmlTexts/GetAttributeTests.cs
@@ -0,0 +1,66 @@
+using NUnit.Framework;
+using YDotNet.Document;
+using YDotNet.Document.Types.XmlTexts;
+
+namespace YDotNet.Tests.Unit.XmlTexts;
+
+public class GetAttributeTests
+{
+    [Test]
+    public void GetAttributeThatDoesNotExistReturnsNull()
+    {
+        // Arrange
+        var (doc, xmlText) = ArrangeDoc();
+
+        // Act
+        var transaction = doc.ReadTransaction();
+        var value = xmlText.GetAttribute(transaction, "size");
+        transaction.Commit();
+
+        // Assert
+        Assert.That(value, Is.Null);
+    }
+
+    [Test]
+    public void GetAttributeThatExistsAndIsEmptyWorks()
+    {
+        // Arrange
+        var (doc, xmlText) = ArrangeDoc();
+
+        // Act
+        var transaction = doc.ReadTransaction();
+        var value = xmlText.GetAttribute(transaction, "empty");
+        transaction.Commit();
+
+        // Assert
+        Assert.That(value, Is.EqualTo(string.Empty));
+    }
+
+    [Test]
+    public void GetAttributeThatExistsAndIsFilledWorks()
+    {
+        // Arrange
+        var (doc, xmlText) = ArrangeDoc();
+
+        // Act
+        var transaction = doc.ReadTransaction();
+        var value = xmlText.GetAttribute(transaction, "number");
+        transaction.Commit();
+
+        // Assert
+        Assert.That(value, Is.EqualTo("7️⃣"));
+    }
+
+    private (Doc, XmlText) ArrangeDoc()
+    {
+        var doc = new Doc();
+        var xmlText = doc.XmlText("xml-text");
+
+        var transaction = doc.WriteTransaction();
+        xmlText.InsertAttribute(transaction, "empty", string.Empty);
+        xmlText.InsertAttribute(transaction, "number", "7️⃣");
+        transaction.Commit();
+
+        return (doc, xmlText);
+    }
+}
