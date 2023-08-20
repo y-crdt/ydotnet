fe2d36c1a1e548c53b8659a2ff02fcf4b755aaa0
Lucas Viana <lv201122@gmail.com>
2023-08-20 14:03:30 -0300

    test(xml-text): add placeholder file for XmlText.ObserveDeep()

---
+++ Tests/YDotNet.Tests.Unit/XmlTexts/ObserveDeepTests.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+
+namespace YDotNet.Tests.Unit.XmlTexts;
+
+[TestFixture]
+[Ignore("The feature must be checked before tests are implemented.")]
+public class ObserveDeepTests
+{
+    [Test]
+    public void ObserveDeepHasPathWhenAddedTextsAndEmbeds()
+    {
+    }
+
+    [Test]
+    public void ObserveDeepHasPathWhenAddedAttributes()
+    {
+    }
+
+    [Test]
+    public void ObserveDeepHasPathWhenUpdatedAttributes()
+    {
+    }
+
+    [Test]
+    public void ObserveDeepHasPathWhenRemovedAttributes()
+    {
+    }
+}
