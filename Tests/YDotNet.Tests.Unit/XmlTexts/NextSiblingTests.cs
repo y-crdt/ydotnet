a7405f85d2c3a2fcf844d99ff27bb0f4371ec6e3
Lucas Viana <lv201122@gmail.com>
2023-08-20 11:41:44 -0300

    test(xml-text): introduce tests for XmlText.NextSibling()

---
+++ Tests/YDotNet.Tests.Unit/XmlTexts/NextSiblingTests.cs
@@ -0,0 +1,74 @@
+using NUnit.Framework;
+using YDotNet.Document;
+using YDotNet.Document.Types.XmlElements;
+
+namespace YDotNet.Tests.Unit.XmlTexts;
+
+public class NextSiblingTests
+{
+    [Test]
+    public void GetsNextSiblingAtBeginning()
+    {
+        // Arrange
+        var (doc, xmlElement) = ArrangeDoc();
+
+        // Act
+        var transaction = doc.ReadTransaction();
+        var target = xmlElement.Get(transaction, index: 0).XmlText;
+        var sibling = target.NextSibling(transaction);
+        transaction.Commit();
+
+        // Assert
+        Assert.That(sibling.XmlElement, Is.Not.Null);
+        Assert.That(sibling.XmlText, Is.Null);
+    }
+
+    [Test]
+    public void GetsNextSiblingAtMiddle()
+    {
+        // Arrange
+        var (doc, xmlElement) = ArrangeDoc();
+
+        // Act
+        var transaction = doc.ReadTransaction();
+        var target = xmlElement.Get(transaction, index: 2).XmlText;
+        var sibling = target.NextSibling(transaction);
+        transaction.Commit();
+
+        // Assert
+        Assert.That(sibling.XmlElement, Is.Not.Null);
+        Assert.That(sibling.XmlText, Is.Null);
+    }
+
+    [Test]
+    public void GetsNextSiblingAtEnding()
+    {
+        // Arrange
+        var (doc, xmlElement) = ArrangeDoc();
+
+        // Act
+        var transaction = doc.ReadTransaction();
+        var target = xmlElement.Get(transaction, index: 4).XmlText;
+        var sibling = target.NextSibling(transaction);
+        transaction.Commit();
+
+        // Assert
+        Assert.That(sibling, Is.Null);
+    }
+
+    private (Doc, XmlElement) ArrangeDoc()
+    {
+        var doc = new Doc();
+        var xmlElement = doc.XmlElement("xml-element");
+
+        var transaction = doc.WriteTransaction();
+        xmlElement.InsertText(transaction, index: 0);
+        xmlElement.InsertElement(transaction, index: 1, "width");
+        xmlElement.InsertText(transaction, index: 2);
+        xmlElement.InsertElement(transaction, index: 3, "color");
+        xmlElement.InsertText(transaction, index: 4);
+        transaction.Commit();
+
+        return (doc, xmlElement);
+    }
+}
