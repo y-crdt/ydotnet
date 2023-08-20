cf03182410ccdef761e7048fc9004768d51e5bfc
Lucas Viana <lv201122@gmail.com>
2023-08-20 13:37:03 -0300

    test(xml-text): introduce tests for XmlText.Unobserve()

---
+++ Tests/YDotNet.Tests.Unit/XmlTexts/UnobserveTests.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using YDotNet.Document;
+
+namespace YDotNet.Tests.Unit.XmlTexts;
+
+public class UnobserveTests
+{
+    [Test]
+    public void TriggersWhenXmlTextChangedUntilUnobserved()
+    {
+        // Arrange
+        var doc = new Doc();
+        var xmlText = doc.XmlText("xml-element");
+
+        var called = 0;
+        var subscription = xmlText.Observe(_ => called++);
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        xmlText.Insert(transaction, index: 0, "Lucas");
+        transaction.Commit();
+
+        // Assert
+        Assert.That(called, Is.EqualTo(expected: 1));
+
+        // Act
+        xmlText.Unobserve(subscription);
+
+        transaction = doc.WriteTransaction();
+        xmlText.Insert(transaction, index: 1, " Viana");
+        transaction.Commit();
+
+        // Assert
+        Assert.That(called, Is.EqualTo(expected: 1));
+    }
+}
