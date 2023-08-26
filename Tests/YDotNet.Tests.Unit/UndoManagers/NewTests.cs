3d9ab0a667bf8cea32b312ddb1583db63b9fd2a4
Lucas Viana <lv201122@gmail.com>
2023-08-26 14:29:00 -0300

    test(undo-manager): add tests to create UndoManager without options

--- Tests/YDotNet.Tests.Unit/UndoManagers/NewTests.cs
+++ Tests/YDotNet.Tests.Unit/UndoManagers/NewTests.cs
@@ -7,7 +7,7 @@ namespace YDotNet.Tests.Unit.UndoManagers;
 public class NewTests
 {
     [Test]
-    public void CreateFromDocumentAndText()
+    public void CreateWithOptions()
     {
         // Arrange
         var doc = new Doc();
@@ -19,6 +19,20 @@ public class NewTests
             {
                 CaptureTimeoutMilliseconds = 1000
             });
+
+        // Assert
+        Assert.That(undoManager.Handle, Is.GreaterThan(nint.Zero));
+    }
+
+    [Test]
+    public void CreateWithoutOptions()
+    {
+        // Arrange
+        var doc = new Doc();
+        var text = doc.Text("text");
+
+        // Act
+        var undoManager = new UndoManager(doc, text);
 
         // Assert
         Assert.That(undoManager.Handle, Is.GreaterThan(nint.Zero));
