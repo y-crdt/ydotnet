23330c18ce6a73b6e34a4c8d4a5dcea52addafd9
Lucas Viana <lv201122@gmail.com>
2023-08-25 22:55:06 -0300

    feat(undo-manager): introduce the initial version of the UndoManagerChannel

---
+++ YDotNet/Native/UndoManager/UndoManagerChannel.cs
@@ -0,0 +1,9 @@
+using System.Runtime.InteropServices;
+
+namespace YDotNet.Native.UndoManager;
+
+internal static class UndoManagerChannel
+{
+    [DllImport(ChannelSettings.NativeLib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "yundo_manager")]
+    public static extern nint NewWithOptions(nint doc, nint branch, nint options);
+}
