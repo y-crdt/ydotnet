9acc00b9fe474525e2a9331451d30905525a4c6b
Lucas Viana <lv201122@gmail.com>
2023-08-27 18:30:55 -0300

    test(undo-manager): introduce tests for Undo() on Text instances

--- Tests/YDotNet.Tests.Unit/UndoManagers/UndoTests.cs
+++ Tests/YDotNet.Tests.Unit/UndoManagers/UndoTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using YDotNet.Document;
+using YDotNet.Document.Cells;
 using YDotNet.Document.UndoManagers;
 
 namespace YDotNet.Tests.Unit.UndoManagers;
@@ -22,9 +23,62 @@ public class UndoTests
     }
 
     [Test]
-    [Ignore("Waiting to be implemented.")]
     public void UndoAddingAndUpdatingAndRemovingContentOnText()
     {
+        // Arrange
+        var doc = new Doc();
+        var text = doc.Text("text");
+        var undoManager = new UndoManager(doc, text, new UndoManagerOptions { CaptureTimeoutMilliseconds = 0 });
+        var transaction = doc.WriteTransaction();
+        text.Insert(transaction, index: 0, "Lucas");
+        transaction.Commit();
+
+        // Act (add text and undo)
+        transaction = doc.WriteTransaction();
+        text.Insert(transaction, index: 0, " Viana");
+        transaction.Commit();
+        var result = undoManager.Undo();
+
+        transaction = doc.ReadTransaction();
+        var value = text.String(transaction);
+        transaction.Commit();
+
+        // Assert
+        Assert.That(value, Is.EqualTo("Lucas"));
+        Assert.That(result, Is.True);
+
+        // Act (add embed and undo)
+        transaction = doc.WriteTransaction();
+        text.InsertEmbed(
+            transaction, index: 3, Input.Boolean(value: true), Input.Object(
+                new Dictionary<string, Input>
+                {
+                    { "bold", Input.Boolean(value: true) }
+                }));
+        transaction.Commit();
+        result = undoManager.Undo();
+
+        transaction = doc.ReadTransaction();
+        var chunks = text.Chunks(transaction).ToArray();
+        transaction.Commit();
+
+        // Assert
+        Assert.That(chunks.Length, Is.EqualTo(expected: 1));
+        Assert.That(result, Is.True);
+
+        // Act (remove and undo)
+        transaction = doc.WriteTransaction();
+        text.RemoveRange(transaction, index: 2, length: 2);
+        transaction.Commit();
+        result = undoManager.Undo();
+
+        transaction = doc.ReadTransaction();
+        value = text.String(transaction);
+        transaction.Commit();
+
+        // Assert
+        Assert.That(value, Is.EqualTo("Lucas"));
+        Assert.That(result, Is.True);
     }
 
     [Test]
