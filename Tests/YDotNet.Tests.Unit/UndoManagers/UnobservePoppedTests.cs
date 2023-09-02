590a3f28d9444b839d8fed9f972d8db71c36ed0f
Lucas Viana <lv201122@gmail.com>
2023-09-01 23:44:47 -0300

    test(undo-manager): introduce tests for UnobservePopped()

---
+++ Tests/YDotNet.Tests.Unit/UndoManagers/UnobservePoppedTests.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using YDotNet.Document;
+using YDotNet.Document.UndoManagers;
+using YDotNet.Document.UndoManagers.Events;
+
+namespace YDotNet.Tests.Unit.UndoManagers;
+
+public class UnobservePoppedTests
+{
+    [Test]
+    public void TriggersWhenChangesUntilUnobserved()
+    {
+        // Arrange
+        var doc = new Doc();
+        var text = doc.Text("text");
+        var undoManager = new UndoManager(doc, text, new UndoManagerOptions { CaptureTimeoutMilliseconds = 0 });
+
+        UndoEvent? undoEvent = null;
+        var subscription = undoManager.ObservePopped(e => undoEvent = e);
+
+        // Act
+        var transaction = doc.WriteTransaction();
+        text.Insert(transaction, index: 0, "Lucas");
+        transaction.Commit();
+        undoManager.Undo();
+
+        // Assert
+        Assert.That(undoEvent, Is.Not.Null);
+
+        // Act
+        undoEvent = null;
+        undoManager.UnobservePopped(subscription);
+        undoManager.Redo();
+
+        // Assert
+        Assert.That(undoEvent, Is.Null);
+    }
+}
