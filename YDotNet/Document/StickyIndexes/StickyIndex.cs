582c1732c55b8930a205a99c3adf27ce940578ea
Lucas Viana <lv201122@gmail.com>
2023-09-07 19:23:01 -0300

    feat(sticky-index): introduce initial version of the StickyIndex class

---
+++ YDotNet/Document/StickyIndexes/StickyIndex.cs
@@ -0,0 +1,29 @@
+using YDotNet.Document.Types.Branches;
+
+namespace YDotNet.Document.StickyIndexes;
+
+/// <summary>
+///     A sticky index keeps track of the number index of positions inside <see cref="Branch" /> instances
+///     even when changes happen to the content.
+/// </summary>
+/// <remarks>
+///     For example, placing sticky index before a certain character will always point to this character.
+///     Also, placing a sticky index at the end of a <see cref="Branch" /> will always point to the end of that
+///     <see cref="Branch" />.
+/// </remarks>
+public class StickyIndex
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="StickyIndex" /> class.
+    /// </summary>
+    /// <param name="handle">The handle to the native resource.</param>
+    internal StickyIndex(nint handle)
+    {
+        Handle = handle;
+    }
+
+    /// <summary>
+    ///     Gets the handle to the native resource.
+    /// </summary>
+    internal nint Handle { get; }
+}
