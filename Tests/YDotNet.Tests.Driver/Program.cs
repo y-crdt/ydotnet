2bb7289249040d49a23ce5cc6f08515b617b17d5
Lucas Viana <lv201122@gmail.com>
2023-09-25 22:01:40 -0300

    perf(memory-leaks): introduce Tasks.Texts.Create

--- Tests/YDotNet.Tests.Driver/Program.cs
+++ Tests/YDotNet.Tests.Driver/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using YDotNet.Tests.Driver.Tasks.Docs;
+using YDotNet.Tests.Driver.Tasks.Texts;
 
-new Clone().Run();
+new Create().Run();
