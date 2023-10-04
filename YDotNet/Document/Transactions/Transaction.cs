3e22af0918ba3b81d210ddbc561b776e6773cc42
Sebastian Stehle <sebastian@squidex.io>
2023-10-04 17:26:20 +0200

    Update Transaction.cs
    The state vector is optional.

--- YDotNet/Document/Transactions/Transaction.cs
+++ YDotNet/Document/Transactions/Transaction.cs
@@ -119,15 +119,15 @@ public class Transaction : IDisposable
     ///     </para>
     /// </remarks>
     /// <param name="stateVector">
-    ///     The state vector to be used as base for comparison and generation of the difference.
+    ///     The optional state vector to be used as base for comparison and generation of the difference.
     /// </param>
     /// <returns>
     ///     The lib0 v1 encoded state difference between the <see cref="Doc" /> of this <see cref="Transaction" /> and the
     ///     remote <see cref="Doc" />.
     /// </returns>
-    public byte[] StateDiffV1(byte[] stateVector)
+    public byte[] StateDiffV1(byte[]? stateVector)
     {
-        var handle = TransactionChannel.StateDiffV1(Handle, stateVector, (uint) stateVector.Length, out var length);
+        var handle = TransactionChannel.StateDiffV1(Handle, stateVector, (uint) (stateVector != null ? stateVector.Length : 0), out var length);
         var data = MemoryReader.ReadBytes(handle, length);
         BinaryChannel.Destroy(handle, length);
 
@@ -152,15 +152,15 @@ public class Transaction : IDisposable
     ///     </para>
     /// </remarks>
     /// <param name="stateVector">
-    ///     The state vector to be used as base for comparison and generation of the difference.
+    ///     The optional state vector to be used as base for comparison and generation of the difference.
     /// </param>
     /// <returns>
     ///     The lib0 v2 encoded state difference between the <see cref="Doc" /> of this <see cref="Transaction" /> and the
     ///     remote <see cref="Doc" />.
     /// </returns>
-    public byte[] StateDiffV2(byte[] stateVector)
+    public byte[] StateDiffV2(byte[]? stateVector)
     {
-        var handle = TransactionChannel.StateDiffV2(Handle, stateVector, (uint) stateVector.Length, out var length);
+        var handle = TransactionChannel.StateDiffV2(Handle, stateVector, (uint) (stateVector != null ? stateVector.Length : 0), out var length);
         var data = MemoryReader.ReadBytes(handle, length);
         BinaryChannel.Destroy(handle, length);
 
