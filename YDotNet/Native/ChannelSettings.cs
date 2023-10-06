393d462abd249d5224f01974110799c5be39e008
Sebastian <sebastian@squidex.io>
2023-10-06 12:02:15 +0200

    Fix library name.

--- YDotNet/Native/ChannelSettings.cs
+++ YDotNet/Native/ChannelSettings.cs
@@ -2,9 +2,6 @@ namespace YDotNet.Native;
 
 internal static class ChannelSettings
 {
-#if WINDOWS
-    public const string NativeLib = "yrs.dll";
-#else
-    public const string NativeLib = "libyrs.dylib";
-#endif
+    // https://learn.microsoft.com/en-us/dotnet/standard/native-interop/native-library-loading
+    public const string NativeLib = "yrs";
 }
