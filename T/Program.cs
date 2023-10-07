a650a5f875ee6bdcc572fa618b6d1ac011625640
Sebastian <sebastian@squidex.io>
2023-10-07 15:02:41 +0200

    simple run

---
+++ T/Program.cs
@@ -0,0 +1,28 @@
+using YDotNet.Document;
+using YDotNet.Document.Cells;
+
+namespace T
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arrange
+            var doc = new Doc();
+            var array1 = doc.Array("array-1");
+
+            var transaction = doc.WriteTransaction();
+            array1.InsertRange(
+                transaction, index: 0, new[]
+                {
+                Input.Long(value: 2469L),
+                Input.Null(),
+                Input.Boolean(value: false),
+                Input.Map(new Dictionary<string, Input>())
+                });
+
+            var map2 = array1.Get(transaction, index: 3).Map;
+            map2.Insert(transaction, "array-3", Input.Array(Array.Empty<Input>()));
+        }
+    }
+}
