90954c45b5612d69d2445059ce6264255c564fbc
Lucas Viana <lv201122@gmail.com>
2023-10-10 23:12:23 -0300

    docs(native): improve comment in ChannelSettings

--- YDotNet/Native/ChannelSettings.cs
+++ YDotNet/Native/ChannelSettings.cs
@@ -2,6 +2,8 @@ namespace YDotNet.Native;
 
 internal static class ChannelSettings
 {
-    // https://learn.microsoft.com/en-us/dotnet/standard/native-interop/native-library-loading
+    // The file extension doesn't need to be defined here because the `DllImport` attribute adds it automatically.
+    //
+    // More information: https://learn.microsoft.com/en-us/dotnet/standard/native-interop/native-library-loading
     public const string NativeLib = "yrs";
 }
