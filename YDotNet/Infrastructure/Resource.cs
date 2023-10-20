03bb43279ca5465fcce96e9ee0aff364d6091eab
Lucas Viana <lv201122@gmail.com>
2023-10-20 00:15:26 -0300

    docs(improvements): fix typo, format, and improve documentation for the `Resource` class

--- YDotNet/Infrastructure/Resource.cs
+++ YDotNet/Infrastructure/Resource.cs
@@ -1,24 +1,24 @@
 namespace YDotNet.Infrastructure;
 
 /// <summary>
-/// Base class for all resoruces.
+///     Base class for managed resources that hold native resources.
 /// </summary>
 public abstract class Resource : IDisposable
 {
     /// <summary>
-    /// Gets a value indicating whether this instance is disposed.
+    ///     Gets a value indicating whether this instance is disposed.
     /// </summary>
     public bool IsDisposed { get; private set; }
 
-    /// <inheritdoc/>
+    /// <inheritdoc />
     public void Dispose()
     {
-        Dispose(true);
+        Dispose(disposing: true);
         GC.SuppressFinalize(this);
     }
 
     /// <summary>
-    /// Throws an exception if this object or the owner is disposed.
+    ///     Throws an exception if this object or the owner is disposed.
     /// </summary>
     /// <exception cref="ObjectDisposedException">Object or the owner has been disposed.</exception>
     protected void ThrowIfDisposed()
@@ -30,9 +30,9 @@ public abstract class Resource : IDisposable
     }
 
     /// <summary>
-    /// Releases all unmanaged resources.
+    ///     Releases all unmanaged resources.
     /// </summary>
-    /// <param name="disposing">True, if also managed resources should be disposed.</param>
+    /// <param name="disposing"><c>true</c> if managed resources should be disposed as well.</param>
     protected void Dispose(bool disposing)
     {
         if (IsDisposed)
@@ -41,12 +41,13 @@ public abstract class Resource : IDisposable
         }
 
         DisposeCore(disposing);
+
         IsDisposed = true;
     }
 
     /// <summary>
-    /// Releases all unmanaged resources.
+    ///     Releases all unmanaged resources.
     /// </summary>
-    /// <param name="disposing">True, if also managed resources should be disposed.</param>
+    /// <param name="disposing"><c>true</c> if managed resources should be disposed as well.</param>
     protected internal abstract void DisposeCore(bool disposing);
 }
