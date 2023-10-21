b66afb3eaf1daf768c4689faadd3f9422211a6b2
Lucas Viana <lv201122@gmail.com>
2023-10-21 11:41:56 -0300

    refactor(improvements): drop `ThrowHelper.YDotNetException()` since it's better to throw exceptions inline for flow analysis

--- YDotNet/Infrastructure/ThrowHelper.cs
+++ YDotNet/Infrastructure/ThrowHelper.cs
@@ -4,16 +4,11 @@ internal static class ThrowHelper
 {
     public static void Null()
     {
-        YDotNetException("Operation failed. The yffi library returned null without further details.");
+        throw new YDotNetException("Operation failed. The yffi library returned null without further details.");
     }
 
     public static void PendingTransaction()
     {
-        YDotNetException("Failed to open a transaction, probably because another transaction is still open.");
-    }
-
-    public static void YDotNetException(string message)
-    {
-        throw new YDotNetException(message);
+        throw new YDotNetException("Failed to open a transaction, probably because another transaction is still open.");
     }
 }
