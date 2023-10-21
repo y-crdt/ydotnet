95d2828c41ec2203bddb5952277e139835631996
Lucas Viana <lv201122@gmail.com>
2023-10-21 11:38:01 -0300

    refactor(improvements): format, move, and simplify `ThrowHelper`

---
+++ YDotNet/Infrastructure/ThrowHelper.cs
@@ -0,0 +1,19 @@
+namespace YDotNet.Infrastructure;
+
+internal static class ThrowHelper
+{
+    public static void InternalError()
+    {
+        YDotNetException("Operation failed. The yffi library returned null without further details.");
+    }
+
+    public static void PendingTransaction()
+    {
+        YDotNetException("Failed to open a transaction, probably because another transaction is still open.");
+    }
+
+    public static void YDotNetException(string message)
+    {
+        throw new YDotNetException(message);
+    }
+}
