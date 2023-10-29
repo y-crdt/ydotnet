51b9a40a2d90ab1e555def359d731b1c9f99cd49
Lucas Viana <lv201122@gmail.com>
2023-10-29 18:10:50 -0300

    refactor(improvements): make `TypeBase` extend from `UnmanagedResource`

--- YDotNet/Infrastructure/TypeBase.cs
+++ YDotNet/Infrastructure/TypeBase.cs
@@ -1,36 +1,18 @@
 namespace YDotNet.Infrastructure;
 
 /// <summary>
-/// Base class for all managed types.
+///     Base class for all managed types.
 /// </summary>
-public abstract class TypeBase : ITypeBase
+public abstract class TypeBase : UnmanagedResource
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="TypeBase"/> class.
+    ///     Initializes a new instance of the <see cref="TypeBase" /> class.
     /// </summary>
+    /// <param name="handle">A handle to the unmanaged resource.</param>
     /// <param name="isDisposed">A value indicating if the instance is deleted.</param>
-    protected TypeBase(bool isDisposed)
+    protected TypeBase(nint handle, bool isDisposed)
+        : base(handle)
     {
         IsDisposed = isDisposed;
-    }
-
-    /// <inheritdoc/>
-    public bool IsDisposed { get; private set; }
-
-    /// <summary>
-    /// Throws an exception if the type is disposed.
-    /// </summary>
-    protected void ThrowIfDisposed()
-    {
-        if (IsDisposed)
-        {
-            throw new ObjectDisposedException(GetType().Name);
-        }
-    }
-
-    /// <inheritdoc/>
-    public void MarkDisposed()
-    {
-        IsDisposed = true;
     }
 }
