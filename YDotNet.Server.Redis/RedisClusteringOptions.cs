8806f6ef547fd7930369353eaf73eefcfe8b9cd1
Sebastian <sebastian@squidex.io>
2023-11-15 12:39:47 +0100

    Add logo to all assets. Derived from y-crdt logo with .NET font family.

--- YDotNet.Server.Redis/RedisClusteringOptions.cs
+++
@@ -1,8 +0,0 @@
-using StackExchange.Redis;
-
-namespace YDotNet.Server.Redis;
-
-public sealed class RedisClusteringOptions
-{
-    public RedisChannel Channel { get; set; } = RedisChannel.Literal("YDotNet");
-}
