8e27e8810479e2ee15f3b184dfde992ecb07bb97
Lucas Viana <lv201122@gmail.com>
2024-03-23 08:40:36 -0300

    test: introduce tests for `Branch.Id()`

---
+++ Tests/YDotNet.Tests.Unit/Branches/IdTests.cs
@@ -0,0 +1,77 @@
+using NUnit.Framework;
+using YDotNet.Document;
+using YDotNet.Document.Cells;
+using YDotNet.Document.Options;
+using YDotNet.Document.Types.Branches;
+
+namespace YDotNet.Tests.Unit.Branches;
+
+public class IdTests
+{
+    [Test]
+    public void ReturnsIdForRootLevelType()
+    {
+        // Arrange
+        var doc = new Doc();
+        var branch = doc.Text("text");
+
+        // Act
+        var id = branch.Id();
+
+        // Assert
+        Assert.That(id, Is.Not.Null);
+        Assert.That(id.HasLengthAndName, Is.True);
+        Assert.That(id.HasClientIdAndClock, Is.False);
+        Assert.That(id.Length, Is.EqualTo(expected: 4));
+        Assert.That(id.Name, Is.EqualTo("text"));
+        Assert.That(id.ClientId, Is.EqualTo(expected: null));
+        Assert.That(id.Clock, Is.EqualTo(expected: null));
+    }
+
+    [Test]
+    public void ReturnsIdForNestedType()
+    {
+        // Arrange
+        var doc = new Doc(
+            new DocOptions
+            {
+                Id = 37
+            });
+        var map = doc.Map("map");
+
+        var transaction = map.WriteTransaction();
+        map.Insert(transaction, "text", Input.Text(""));
+        Branch branch = map.Get(transaction, "text")!.Text;
+        transaction.Commit();
+
+        // Act
+        var id = branch.Id();
+
+        // Assert
+        Assert.That(id, Is.Not.Null);
+        Assert.That(id.HasLengthAndName, Is.False);
+        Assert.That(id.HasClientIdAndClock, Is.True);
+        Assert.That(id.Length, Is.Null);
+        Assert.That(id.Name, Is.Null);
+        Assert.That(id.ClientId, Is.EqualTo(expected: 37));
+        Assert.That(id.Clock, Is.EqualTo(expected: 0));
+
+        // Arrange
+        transaction = map.WriteTransaction();
+        map.Insert(transaction, "array", Input.Array(Array.Empty<Input>()));
+        branch = map.Get(transaction, "array")!.Array;
+        transaction.Commit();
+
+        // Act
+        id = branch.Id();
+
+        // Assert
+        Assert.That(id, Is.Not.Null);
+        Assert.That(id.HasLengthAndName, Is.False);
+        Assert.That(id.HasClientIdAndClock, Is.True);
+        Assert.That(id.Length, Is.Null);
+        Assert.That(id.Name, Is.Null);
+        Assert.That(id.ClientId, Is.EqualTo(expected: 37));
+        Assert.That(id.Clock, Is.EqualTo(expected: 1));
+    }
+}
