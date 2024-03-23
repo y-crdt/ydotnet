80bcf59022355c19afb9f82f0d3b183ac15cfde3
Lucas Viana <lv201122@gmail.com>
2024-03-23 08:51:55 -0300

    docs: add documentation for `BranchId` class and properties

--- YDotNet/Document/Types/Branches/BranchId.cs
+++ YDotNet/Document/Types/Branches/BranchId.cs
@@ -3,6 +3,9 @@ using YDotNet.Native.Types.Branches;
 
 namespace YDotNet.Document.Types.Branches;
 
+/// <summary>
+///     Represents a logical identifier for a shared collection.
+/// </summary>
 public class BranchId
 {
     internal BranchId(BranchIdNative native)
@@ -12,13 +15,30 @@ public class BranchId
 
     private BranchIdNative Native { get; }
 
+    /// <summary>
+    ///     <para>Gets a value indicating whether <see cref="ClientId" /> and <see cref="Clock" /> have values.</para>
+    ///     <para>If this is <c>false</c>, check <see cref="HasName" />.</para>
+    /// </summary>
     public bool HasClientIdAndClock => Native.ClientIdOrLength > 0;
 
+    /// <summary>
+    ///     Gets a value indicating whether <see cref="Name" /> has values.
+    ///     <para>If this is <c>false</c>, check <see cref="HasClientIdAndClock" />.</para>
+    /// </summary>
     public bool HasName => !HasClientIdAndClock;
 
+    /// <summary>
+    ///     Gets the client ID of a creator of a nested shared type that this <see cref="BranchId" /> points to.
+    /// </summary>
     public long? ClientId => HasClientIdAndClock ? Native.ClientIdOrLength : null;
 
+    /// <summary>
+    ///     Gets the clock number timestamp when the creator of a nested shared type created it.
+    /// </summary>
     public uint? Clock => HasClientIdAndClock ? Native.BranchIdVariant.Clock : null;
 
+    /// <summary>
+    ///     Gets the name of the root-level shared type.
+    /// </summary>
     public string? Name => HasClientIdAndClock ? null : MemoryReader.ReadUtf8String(Native.BranchIdVariant.NamePointer);
 }
